309d124bf9770560b4add9d960c3f8e053517343
AaronFlower <haojunzhan@gmail.com>
2017-06-28 11:56:17 +0800

    second step: make the route to be a factory function

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import {createRoute} from './router'
 /**
  * 项目程序需要一个工厂，router, store 也是一样的。
  */
 
 export function createApp (context) {
+  let router = createRoute()
+
   const app = new Vue({
+    router,
     render: h => h(App)
   })
   return {app}
--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,15 @@ import Hello from '@/components/Hello'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
-})
+export function createRoute () {
+  const router = new Router({
+    routes: [
+      {
+        path: '/',
+        name: 'Hello',
+        component: Hello
+      }
+    ]
+  })
+  return router
+}
